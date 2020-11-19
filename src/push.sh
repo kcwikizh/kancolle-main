8500163a681956845ff2cd9b3f3e21396bec677a
Gizeta <0w0@gizeta.tk>
2020-11-19 13:20:12 +0800

    Update push.sh

--- src/push.sh
+++ src/push.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-export GIT_AUTHOR_NAME="Botkaze"
-export GIT_AUTHOR_EMAIL="gkpyon+1@gmail.com"
+export GIT_AUTHOR_NAME="Gizeta"
+export GIT_AUTHOR_EMAIL="0w0@gizeta.tk"
 export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
 export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
 
 git checkout master
 git add .
 git commit -m "chore: autobuild for $(cat dist/version)"
-git push "https://${GITHUB_TOKEN}@github.com/kcwiki/kancolle-main.git" master
+git push "https://${GITHUB_TOKEN}@github.com/kcwikizh/kancolle-main.git" master
