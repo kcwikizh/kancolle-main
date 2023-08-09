f78b33f9dc84c3169320c671854ffa7505687028
Gizeta <i@gizeta.me>
2023-08-10 02:05:51 +0800

    Update decode.js

--- src/decode.js
+++ src/decode.js
@@ -23,5 +23,6 @@ writeFileSync(
     .replace(/\[\('([_a-zA-Z][_a-zA-Z0-9]*?)'\)\]/g, "['$1']")
     .replace(/_(0x[a-f0-9]{4,6})/g, (_, x) => `_${(vars[x] || (vars[x] = ++i)).toString(36)}`)
     .replace(/(0x[a-f0-9]+?)\.toString()/g, (_, x) => `(${+x}).toString()`)
-    .replace(/(0x[a-f0-9]+)/g, (_, x) => `${+x}`),
+    .replace(/(0x[a-f0-9]+)/g, (_, x) => `${+x}`)
+    .replace("`'symbol'`", "`symbol`"),
 )
