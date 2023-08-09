eee11a3021a057337f7e4ad69ba23dc94e8c7e12
Gizeta <0w0@gizeta.tk>
2023-08-09 18:07:10 +0000

    chore: autobuild for 5.7.0.0

--- src/decode.js
+++ src/decode.js
@@ -24,5 +24,5 @@ writeFileSync(
     .replace(/_(0x[a-f0-9]{4,6})/g, (_, x) => `_${(vars[x] || (vars[x] = ++i)).toString(36)}`)
     .replace(/(0x[a-f0-9]+?)\.toString()/g, (_, x) => `(${+x}).toString()`)
     .replace(/(0x[a-f0-9]+)/g, (_, x) => `${+x}`)
-    .replace("`'symbol'`", "`symbol`"),
+    .replace("`'symbol'`", '`symbol`'),
 )
